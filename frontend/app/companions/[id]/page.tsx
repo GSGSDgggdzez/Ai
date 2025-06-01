7f2b3f10a506a160e9cbfdb5385c8a24f649f4b7
harold <harolddefree6@gmail.com>
2025-06-01 09:59:24 +0200

    initial

---
+++ frontend/app/companions/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const companionsSection = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default companionsSection
---
+++ frontend/app/companions/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const companionsLibrary = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default companionsLibrary
